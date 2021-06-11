3493e79587a04da3fab28d78a56e2b2ef73daa9e
Spencer Rogers <spencerjrogers@Spencers-M1-MacBook-Pro.local>
2021-06-11 14:53:18 -0400

    Changed Tides Tracker to a cc for long rest resets

--- Completed/tidesreset.py
+++ Completed/tidesreset.py
@@ -1,20 +1,22 @@
 !alias tides embed
 <drac2>
-character().set_cvar_nx('tidesReset','0')
-diceTable = {'0':'1d4', '1':'1d3', '2':'1d2'}
-currentDie = diceTable[tidesReset] if int(tidesReset)<3 else '1d2'
+cc = "Tides Tracker"
+character().create_cc_nx(cc,0,reset='long',reset_to='0',dispType=None)
+diceTable = {0:'1d4', 1:'1d3', 2:'1d2'}
+tryCounter = character().get_cc(cc)
+currentDie = diceTable[tryCounter] if tryCounter<3 else '1d2'
 theRoll = roll(currentDie)
-tryCounter = int(tidesReset)
+
 
 if '%1%' == 'reset':
-	character().set_cvar('tidesReset',"0")
-	return f'''embed -title "Tides of Chaos reset!" -desc "{name} manually reset the " '''
+	character().set_cc(cc,0)
+	return f'''embed -title "Tides of Chaos reset!" -desc "Counter manually reset" '''
 elif theRoll==1:
-	character().set_cvar('tidesReset','0')
+	character().set_cc(cc,0)
 	return f'''embed -title "Tides of Chaos reset!" -desc "{name} rolled a {theRoll} on a {currentDie}. Run !wmsurge tides now." '''
 else:
 	tryCounter+=1
-	character().set_cvar('tidesReset',str(tryCounter))
-	return f'''embed -title "Tides of Chaos does not reset!" -desc "{name} rolled a {theRoll} on a {currentDie}. This was attempt {tidesReset}." '''
+	character().mod_cc(cc,+1)
+	return f'''embed -title "Tides of Chaos does not reset!" -desc "{name} rolled a {theRoll} on a {currentDie}. This was attempt {tryCounter}." '''
 </drac2>
 -footer "!tides"
